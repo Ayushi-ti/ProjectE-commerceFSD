8c9c2d1489c43d6d8e2c28cee19f8bc8ee00cf2d
Ayushi <AYUSHI.ayushi@ibm.com>
2021-04-11 20:13:53 +0530

    services updated

--- Client-side/ecommerce-frontEnd/src/core/services/customer.service.ts
+++ Client-side/ecommerce-frontEnd/src/core/services/customer.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
+  private data:any[] = [];
 
-  constructor() { }
+  public set customers(customers){
+    this.data = customers;
+  }
+
+  public get customers(){
+    return this.data;
+  }
+
+  private host:string = "http://localhost:8080";
+
+  constructor(private http: HttpClient) { }
+
+  getCustomers(){
+    return this.http.get(`${this.host}/customer/show`);
+  }
+  saveCustomer(customer){
+    return this.http.post(`${this.host}/customer/register`, customer);
+  }
+
+ // deleteCustomer(id){
+ //   return this.http.delete(`${this.host}/customer/${id}`);
+ // }
+
+  findCustomerById(id){
+    return this.http.get(`${this.host}/customer/${id}`);
+  }
+
+  editCustomer(id, newCustomer){
+    return this.http.put(`${this.host}/books/${id}`, newCustomer);
+  }
 }
