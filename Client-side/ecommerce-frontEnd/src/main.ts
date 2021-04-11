f4205acf04e290aa14e00e8b5060eba3a3ce52ec
Ayushi <AYUSHI.ayushi@ibm.com>
2021-04-11 14:26:17 +0530

    updated

--- Client-side/ecommerce-frontEnd/src/main.ts
+++ Client-side/ecommerce-frontEnd/src/main.ts
@@ -3,10 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 
-<<<<<<< HEAD
-=======
-import { AppModule } from './app.module';
->>>>>>> 507314c2ef885c9186bf397fb12ef179eb802f52
 import { environment } from './environments/environment';
 
 if (environment.production) {
