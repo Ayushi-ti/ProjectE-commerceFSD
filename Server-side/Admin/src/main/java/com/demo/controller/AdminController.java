18bda23e9dc43ccb8661366b98a6e79ac7085275
Shravya Kadam <shravyakadam97@gmail.com>
2021-04-14 14:45:58 +0530

    admin login

--- Server-side/Admin/src/main/java/com/demo/controller/AdminController.java
+++ Server-side/Admin/src/main/java/com/demo/controller/AdminController.java
@@ -9,6 +9,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -90,7 +91,11 @@ public class AdminController {
 		users.add(user);
 		return users;
 	}
-	
+	@GetMapping("/login/{email}/{password}")
+	public boolean loginadmin(@PathVariable String email,@PathVariable String password)
+	{
+		return adminService.verifyadmin(email, password);
+	}
 	
 
 
--- Server-side/Admin/src/main/java/com/demo/service/AdminService.java
+++ Server-side/Admin/src/main/java/com/demo/service/AdminService.java
@@ -3,6 +3,7 @@ package com.demo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import com.demo.repositories.CustomerRepository;
 import com.demo.repositories.ProductRepository;
 
@@ -13,6 +14,16 @@ public class AdminService {
 	ProductRepository productRepository;
 	@Autowired
 	CustomerRepository customerRepository;
+	
+public boolean  verifyadmin( String email,String password)
+	
+	{
+	    if(email=="admin@gmail.com" && password=="admin")
+	    {
+	    	return true;
+	    }
+	    return false;
+	}
 	
 
 }
