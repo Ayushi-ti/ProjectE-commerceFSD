81f8f8c9bae67ed4315881e84c9ef66bd3ed9be1
Ekta Pant <Ekta.Pant@ibm.com>
2021-04-16 12:48:22 +0530

    orders updated

--- Server-side/Orders/Orders/src/main/java/com/demo/Application.java
+++ Server-side/Orders/Orders/src/main/java/com/demo/Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class OrderApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
