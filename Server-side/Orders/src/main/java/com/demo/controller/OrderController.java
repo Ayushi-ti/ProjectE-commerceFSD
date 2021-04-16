5d4305e2e13898d795a6b44911dd53723ee29aa7
Ayushi-ti <79696938+Ayushi-ti@users.noreply.github.com>
2021-04-16 16:15:41 +0530

    Update OrderController.java
    corrs updated

--- Server-side/Orders/src/main/java/com/demo/controller/OrderController.java
+++ Server-side/Orders/src/main/java/com/demo/controller/OrderController.java
@@ -19,6 +19,7 @@ import com.demo.service.OrderService;
 
 @RestController
 @RequestMapping("orders")
+@CrossOrigin(origins = {"http://localhost:4200"}) 
 public class OrderController {
 	
 	@Autowired
