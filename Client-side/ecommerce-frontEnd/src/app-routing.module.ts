eb81f251882c49409e7cc952c08ca311450a4b38
Ayushi <AYUSHI.ayushi@ibm.com>
2021-04-18 22:13:16 +0530

    product service

--- Client-side/ecommerce-frontEnd/src/app-routing.module.ts
+++ Client-side/ecommerce-frontEnd/src/app-routing.module.ts
@@ -3,25 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 
 
-// const routes: Routes = [
-//   //{ path: '',component: LayoutComponent}; 
-//   {path: '', component: LayoutComponent,
-//   children: [
-//   {path: '',redirectTo: '/home', pathMatch: 'full' },
-//   {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
-//   {path: 'order',loadChildren: () => import('./order/order.module').then(m => m.OrderModule)},
-//   {path: 'auth',loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)}
-//     ]
-//   },
-//   {path: 'admin',loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)},
-//   {path: '', component: LayoutComponent,
-//     children: [
-//    { path: '**',loadChildren: () => import('./home/home.module').then(m => m.HomeModule)}
-//     ]},
-// ];
 
 
-//http://localhost:2400
+
+//http://localhost:4200
 const routes: Routes = [
  // http://localhost:4200/
    {path: '',component: LayoutComponent,
@@ -37,21 +22,7 @@ const routes: Routes = [
   {path: 'admin',loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)}
 ];
  
-  // {
-  //   path: 'auth',
-  //   loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-  // },
-    
-  // {  
-  //   path: 'admin',
-  //  loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
-  // }
-  // {
-  //   //http://:localhost:2400/home automatically directs to home 
-  //   path: '**',
-  //   loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-  // },
-//];
+ 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
