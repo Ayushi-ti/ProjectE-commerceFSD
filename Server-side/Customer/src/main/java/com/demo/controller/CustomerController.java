fc0b354d2e41e364dd971fb12dce456ddfacc8c1
Shravya Kadam <shravyakadam97@gmail.com>
2021-04-19 13:05:30 +0530

    forgot password

--- Server-side/Customer/src/main/java/com/demo/controller/CustomerController.java
+++ Server-side/Customer/src/main/java/com/demo/controller/CustomerController.java
@@ -59,4 +59,9 @@ public class CustomerController {
 	public boolean removecustomer(@PathVariable int id) {
 		return customerService.deletecustomer(id);
 	}
+	@PutMapping("/forgot/{email}")
+	public boolean forgotPassword(@PathVariable String email)
+	{
+		return customerService.updatePassword(email);
+	}
 }
