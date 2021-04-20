a9d930d1c013b5ed3c1bfcc5608e47c1e7e7603f
Ekta Pant <Ekta.Pant@ibm.com>
2021-04-20 13:03:59 +0530

    image

--- Server-side/Product/src/main/java/com/demo/controller/ProductController.java
+++ Server-side/Product/src/main/java/com/demo/controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.demo.controller;
 
+import java.io.IOException;
 import java.util.List;
 
+//import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity.BodyBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +15,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.ByteArrayOutputStream;
+import java.util.Optional;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+import org.springframework.http.HttpStatus;
 
 
+import com.demo.entities.ImageModel;
 import com.demo.entities.Product;
+import com.demo.repositories.ImageRepository;
 import com.demo.service.ProductService;
 
 @RestController
@@ -55,5 +69,116 @@ public class ProductController {
 	public boolean updateProduct(@PathVariable int id,@RequestBody Product product) {
 		return productService.editProduct(id, product);
 	}
+	
+//image
+	
+	@Autowired
+	    ImageRepository imageRepository;
+	
+	    @PostMapping("/upload")
+	
+	    public BodyBuilder uplaodImage(@RequestParam("imageFile") MultipartFile file) throws IOException {
+	
+	
+	        System.out.println("Original Image Byte Size - " + file.getBytes().length);
+	
+	        ImageModel img = new ImageModel(file.getOriginalFilename(), file.getContentType(),
+	
+	                compressBytes(file.getBytes()));
+	
+	       imageRepository.save(img);
+	
+	        return (BodyBuilder) ResponseEntity.status(HttpStatus.OK);
+	
+	    }
+	
+	
+	    @GetMapping(path = { "/get/{imageName}" })
+	
+	    public ImageModel getImage(@PathVariable("imageName") String imageName) throws IOException {
+	
+	        final Optional<ImageModel> retrievedImage = imageRepository.findByName(imageName);
+	
+	        ImageModel img = new ImageModel(retrievedImage.get().getName(), retrievedImage.get().getType(),
+	
+	               decompressBytes(retrievedImage.get().getPicByte()));
+	
+	        return img;
+	
+	    }
+	
+	
+	    // compress the image bytes before storing it in the database
+	
+	    public static byte[] compressBytes(byte[] data) {
+	
+	        Deflater deflater = new Deflater();
+	
+	        deflater.setInput(data);
+	
+	        deflater.finish();
+	
+	        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+	
+	        byte[] buffer = new byte[1024];
+	
+	        while (!deflater.finished()) {
+	
+	            int count = deflater.deflate(buffer);
+	
+	            outputStream.write(buffer, 0, count);
+	
+	        }
+	
+	        try {
+	
+	            outputStream.close();
+	
+	        } catch (IOException e) {
+	
+	        }
+	
+	        System.out.println("Compressed Image Byte Size - " + outputStream.toByteArray().length);
+	
+	        return outputStream.toByteArray();
+	
+	    }
+	
+	
+	    // uncompress the image bytes before returning it to the angular application
+	
+	    public static byte[] decompressBytes(byte[] data) {
+	
+	        Inflater inflater = new Inflater();
+	
+	        inflater.setInput(data);
+	
+	        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+	
+	        byte[] buffer = new byte[1024];
+	
+	        try {
+	
+	            while (!inflater.finished()) {
+	
+	                int count = inflater.inflate(buffer);
+	
+	                outputStream.write(buffer, 0, count);
+	
+	            }
+	
+	            outputStream.close();
+	
+	        } catch (IOException ioe) {
+	
+	        } catch (DataFormatException e) {
+	
+	        }
+	
+	        return outputStream.toByteArray();
+	
+	    }
+	
+	}
 
-}
+
